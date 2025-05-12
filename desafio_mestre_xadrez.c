fea563a30eb8e8a3b796bfb8d3041c3fdc7510d9
João Emanuel <166662967+joaoemanuelrocha@users.noreply.github.com>
2025-05-12 17:26:35 -0300

    Update desafio_mestre_xadrez.c

--- desafio_mestre_xadrez.c
+++ desafio_mestre_xadrez.c
@@ -6,14 +6,14 @@ void moverTorre(int casas) {
         moverTorre(casas - 1);
     }
 }
-
+// Movimento da torre usando recursividade
 void moverRainha(int casas){
     if (casas > 0) {
         printf("Esquerda \n");
         moverRainha(casas - 1);
     }
 }
-
+// Movimento da rainha usando recursividade
 void moverBispo(int casas){
     if (casas > 0) {
         for (int i = 0; i < 1; i++){
@@ -26,17 +26,17 @@ void moverBispo(int casas){
         moverBispo(casas - 1);
     }
 }
-
+// Movimento do Bispo usando recursividade e loops aninhados
 int main() {
     printf("Movimento das Torres \n");
     moverTorre(5);
-
+    // Quantidade de movimento da torre
     printf("Movimentos da Rainha \n");
     moverRainha(8);
-
+    // Quantidade de movimento da Rainha
     printf("Movimento do Bispo \n");
     moverBispo(5);
-
+      // Quantidade de movimento do Bispo
     printf("Movimento do Cavalo \n");
     for (int i = 0, j = 1; i < j; i++, j--) {
 
@@ -45,6 +45,6 @@ int main() {
         }
         printf("Direita \n");
     }
-
+    // Movimento do cavalo
     return 0;
 }
